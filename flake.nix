8bfef169c677b8b7bdbe64435570b7088105d199
Lucas Hoffmann <lucc@posteo.de>
2023-05-24 21:00:35 +0200

    Add test script

--- flake.nix
+++ flake.nix
@@ -14,13 +14,20 @@
           [(pkgs.vimPlugins.nvim-treesitter.withPlugins select)];
       };
     in
-    {
+    rec {
       none = nvim (p: []);
       some = nvim (p: [p.lua]);
       all = pkgs.neovim.override {
         configure.packages.treesitter-example.start =
           [pkgs.vimPlugins.nvim-treesitter.withAllGrammars];
+        };
+        default = pkgs.writeShellScriptBin "compare" ''
+          ${pkgs.hyperfine}/bin/hyperfine \
+          -n plain "${pkgs.neovim}/bin/nvim --headless -cq" \
+          -n none "${none}/bin/nvim --headless -cq" \
+          -n some "${some}/bin/nvim --headless -cq" \
+          -n all "${all}/bin/nvim --headless -cq"
+        '';
       };
     };
-  };
-}
+  }
